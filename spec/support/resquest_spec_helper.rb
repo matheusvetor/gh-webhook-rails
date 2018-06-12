38441727704c3413ea95aab8d91fcc4c9e0bee1b
Matheus Oliveira <matheus.vetor@gmail.com>
2018-06-12 16:32:31 -0300

    added some tests stuff

---
+++ spec/support/resquest_spec_helper.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module RequestSpecHelper
+  def json
+    JSON.parse(response.body)
+  end
+end
+
+RSpec.configure do |config|
+  config.include RequestSpecHelper, type: :request
+end
